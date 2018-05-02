29a35f88bbc1cc4f2b0a6f571334d3815cb14adb
liuluyang <liuluyang0312@163.com>
2018-05-02 16:42:34 +0800

    3. navigationView(用于在drawerlayout中设置导航选项)

--- app/src/main/java/robot/boocax/com/materialdesign/MainActivity.java
+++ app/src/main/java/robot/boocax/com/materialdesign/MainActivity.java
@@ -1,10 +1,13 @@
 package robot.boocax.com.materialdesign;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,12 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);//获取navigationview实例
+
         ActionBar actionBar = getSupportActionBar();
         if (null != actionBar) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
 
         }
+        navigationView.setCheckedItem(R.id.nav_call);//将call菜单设置为默认选中
+        //设置菜单项选中事件的监听器.
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_call:
+                        Log.i("navigationView", "nav_call");
+                        break;
+                    case R.id.nav_friends:
+                        Log.i("navigationView", "nav_friends");
+                        break;
+                    case R.id.nav_location:
+                        Log.i("navigationView", "nav_location");
+                        break;
+                    case R.id.nav_mail:
+                        Log.i("navigationView", "nav_mail");
+                        break;
+                    case R.id.nav_task:
+                        Log.i("navigationView", "nav_task");
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
